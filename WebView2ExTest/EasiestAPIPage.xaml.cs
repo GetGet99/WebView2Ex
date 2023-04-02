09f519376ab6396c403807f90faa9f91f182e1c1
getdn <getdna99@gmail.com>
2023-04-01 17:52:04 -0700

    Add Samples

---
+++ WebView2ExTest/EasiestAPIPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using WebView2Ex;
+using Windows.UI;
+using Windows.UI.Core;
+using Windows.UI.Xaml.Controls;
+
+namespace WebView2ExTest;
+
+public sealed partial class EasiestAPIPage : Page
+{
+    public EasiestAPIPage()
+    {
+        InitializeComponent();
+        // Before Runtime is initialized, it will be null
+        wv2ex.RuntimeInitialized += RuntimeInitialized;
+    }
+
+    private void RuntimeInitialized()
+    {
+        // Do whatever with wv2ex.WebView2Runtime
+        wv2ex.WebView2Runtime!.CompositionController!.DefaultBackgroundColor = Colors.Transparent;
+    }
+}
+// Simplified API
+partial class WebView2ExSimplified : WebView2Ex.UI.WebView2Ex
+{
+    public WebView2ExSimplified()
+    {
+        // Assuming we are on core window only
+        SetWindow(CoreWindow.GetForCurrentThread());
+
+        InitializeAsync();
+    }
+    async void InitializeAsync()
+    {
+        // Assuming we create our own runtime
+        WebView2Runtime = await WebView2Runtime.CreateAsync();
+        WebView2Runtime.CoreWebView2!.Navigate(InitialUri);
+        RuntimeInitialized?.Invoke();
+    }
+    public event Action RuntimeInitialized;
+    // I'm lazy to implement normal observable property for Uri,
+    // so I create one for initial Uri for simplicity.
+    // You can technically do this by subscribing to WebView2Runtime.CoreWebView2.SourceChanged event
+    [ObservableProperty]
+    string _InitialUri = "about:blank";
+}
---
+++ WebView2ExTest/PortableRuntimeAndSupportAppWindowPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using WebView2Ex;
+using Windows.UI;
+using Windows.UI.Core;
+using Windows.UI.Xaml.Controls;
+
+namespace WebView2ExTest;
+
+public sealed partial class PortableRuntimeAndSupportAppWindowPage : Page
+{
+    public PortableRuntimeAndSupportAppWindowPage()
+    {
+        InitializeComponent();
+
+        // Set Window, accepts both CoreWindow and AppWindow instance
+        wv2ex.SetWindow(CoreWindow.GetForCurrentThread());
+        
+        InitializeAsync();
+    }
+    async void InitializeAsync()
+    {
+        var environment =
+            await WebView2Environment.CreateAsync();
+            // similar to as CoreWebView2Environment.CreateAsync() but it adds default language
+
+        wv2ex.WebView2Runtime = await WebView2Runtime.CreateAsync(environment);
+
+        // WebView2Runtime reference
+        // WebView2Runtime.CompositionController
+        // WebView2Runtime.CoreWebView2
+        // WebView2Runtime.Environment
+
+        // example usage
+        wv2ex.WebView2Runtime.CompositionController.DefaultBackgroundColor = Colors.Transparent;
+
+
+        wv2ex.WebView2Runtime.CoreWebView2.Navigate("https://www.google.com/");
+    }
+}
